b173cba21ffb9fc36ac3cc18212c044f8a27c9ae
szytel <nate@nextreason.com>
2025-04-01 15:28:51 -0700

    Removed local pem to clear any confusion

--- config.js
+++ config.js
@@ -1,14 +1,8 @@
 // config.js
-// const config = {
-//     clientId: 'your-client-id', // Replace with your client ID
-//     issuer: 'https://your-identity-provider.com', // Replace with your issuer URL (e.g., https://your-next-identity-provider.com)
-//     redirectUri: window.location.origin + '/callback', // Important: Add a callback route
-//     scopes: ['openid', 'profile', 'email'], // Add the scopes you need
-// };
 const config = {
-    clientId: '9d91b1edf0af4d7ab46a07408295ec45', // Replace with your client ID
-    issuer: 'https://ajyc6qopjq-dev.idp.nextidentity.io', // Replace with your issuer URL (e.g., https://your-next-identity-provider.com)
+    clientId: 'your-client-id', // Replace with your client ID
+    issuer: 'https://your-identity-provider.com', // Replace with your issuer URL (e.g., https://your-next-identity-provider.com)
     redirectUri: window.location.origin + '/callback', // Important: Add a callback route
-    scopes: ['profile', 'openid', 'email'], // Add the scopes you need
+    scopes: ['openid', 'profile', 'email'], // Add the scopes you need
 };
 
